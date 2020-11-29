b130488464e33a2e0ca93b4b6fa3aa82cf1128f1
Your Name <you@example.com>
2020-11-29 11:12:56 +0000

    add new page

---
+++ assets/js/26.4f930016.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{369:function(t,s,e){"use strict";e.r(s);var n=e(1),i=Object(n.a)({},function(){var t=this.$createElement,s=this._self._c||t;return s("div",{staticClass:"content"},[s("BlogPostList",{attrs:{list:this.$site.pages}})],1)},[],!1,null,null,null);s.default=i.exports}}]);
